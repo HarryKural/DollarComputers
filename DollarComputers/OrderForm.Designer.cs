993ea6d4388acc751e962a0c219e015413df221e
HarryKural <harrykural333@gmail.com>
2017-03-13 00:33:33 -0400

    Added Order Form ->buttons & group boxes

---
+++ DollarComputers/OrderForm.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace DollarComputers
+{
+    partial class OrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrderForm));
+            this.OrderFormMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SystemComponentsGroupBox = new System.Windows.Forms.GroupBox();
+            this.YourPriceGroupBox = new System.Windows.Forms.GroupBox();
+            this.FinishButton = new System.Windows.Forms.Button();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.OrderFormMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OrderFormMenuStrip
+            // 
+            this.OrderFormMenuStrip.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.OrderFormMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.OrderFormMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.OrderFormMenuStrip.Name = "OrderFormMenuStrip";
+            this.OrderFormMenuStrip.Size = new System.Drawing.Size(717, 27);
+            this.OrderFormMenuStrip.TabIndex = 0;
+            this.OrderFormMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.printToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(41, 23);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("printToolStripMenuItem.Image")));
+            this.printToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.P)));
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.printToolStripMenuItem.Text = "&Print";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(153, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(156, 24);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(44, 23);
+            this.editToolStripMenuItem.Text = "&Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(49, 23);
+            this.helpToolStripMenuItem.Text = "&Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.aboutToolStripMenuItem.Text = "&About...";
+            // 
+            // SystemComponentsGroupBox
+            // 
+            this.SystemComponentsGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.SystemComponentsGroupBox.Location = new System.Drawing.Point(13, 51);
+            this.SystemComponentsGroupBox.Name = "SystemComponentsGroupBox";
+            this.SystemComponentsGroupBox.Size = new System.Drawing.Size(343, 380);
+            this.SystemComponentsGroupBox.TabIndex = 1;
+            this.SystemComponentsGroupBox.TabStop = false;
+            this.SystemComponentsGroupBox.Text = "System Components";
+            // 
+            // YourPriceGroupBox
+            // 
+            this.YourPriceGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.YourPriceGroupBox.Location = new System.Drawing.Point(362, 226);
+            this.YourPriceGroupBox.Name = "YourPriceGroupBox";
+            this.YourPriceGroupBox.Size = new System.Drawing.Size(343, 127);
+            this.YourPriceGroupBox.TabIndex = 2;
+            this.YourPriceGroupBox.TabStop = false;
+            this.YourPriceGroupBox.Text = "Your Price";
+            // 
+            // FinishButton
+            // 
+            this.FinishButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.FinishButton.Location = new System.Drawing.Point(630, 402);
+            this.FinishButton.Name = "FinishButton";
+            this.FinishButton.Size = new System.Drawing.Size(75, 29);
+            this.FinishButton.TabIndex = 3;
+            this.FinishButton.Text = "Finish";
+            this.FinishButton.UseVisualStyleBackColor = true;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.BackButton.Location = new System.Drawing.Point(444, 402);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 29);
+            this.BackButton.TabIndex = 4;
+            this.BackButton.Text = "Back";
+            this.BackButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.CancelButton.Location = new System.Drawing.Point(539, 402);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 29);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(362, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(157, 143);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // OrderForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(717, 443);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.FinishButton);
+            this.Controls.Add(this.YourPriceGroupBox);
+            this.Controls.Add(this.SystemComponentsGroupBox);
+            this.Controls.Add(this.OrderFormMenuStrip);
+            this.MainMenuStrip = this.OrderFormMenuStrip;
+            this.Name = "OrderForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Complete Your Order";
+            this.OrderFormMenuStrip.ResumeLayout(false);
+            this.OrderFormMenuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip OrderFormMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.GroupBox SystemComponentsGroupBox;
+        private System.Windows.Forms.GroupBox YourPriceGroupBox;
+        private System.Windows.Forms.Button FinishButton;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
