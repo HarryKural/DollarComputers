9b046da1ecaa89cccddecce725deb21f8e5db0f8
HarryKural <harrykural333@gmail.com>
2017-03-12 23:19:05 -0400

    Done with UI of Start Form

--- DollarComputers/StartForm.Designer.cs
+++ DollarComputers/StartForm.Designer.cs
@@ -28,20 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.OrderLabel = new System.Windows.Forms.Label();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // OrderLabel
+            // 
+            this.OrderLabel.AutoSize = true;
+            this.OrderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.OrderLabel.Location = new System.Drawing.Point(58, 34);
+            this.OrderLabel.Name = "OrderLabel";
+            this.OrderLabel.Size = new System.Drawing.Size(238, 20);
+            this.OrderLabel.TabIndex = 0;
+            this.OrderLabel.Text = "Order Your Computer Today!";
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.StartButton.Location = new System.Drawing.Point(118, 295);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(128, 29);
+            this.StartButton.TabIndex = 1;
+            this.StartButton.Text = "Start a New Order";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.OpenButton.Location = new System.Drawing.Point(118, 330);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(128, 29);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Open a Saved Order";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.ExitButton.Location = new System.Drawing.Point(118, 365);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(128, 29);
+            this.ExitButton.TabIndex = 3;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::DollarComputers.Properties.Resources.dollarLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
             // 
             // StartForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(356, 406);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.OrderLabel);
             this.Name = "StartForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Start Form";
+            this.Text = "Welcome to Dollar Computer!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label OrderLabel;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
